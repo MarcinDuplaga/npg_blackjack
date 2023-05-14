619b56ce243724f9351b7f65df49fc8c7d6401d1
bratres <95509025+bratres@users.noreply.github.com>
2023-05-14 17:10:37 +0200

    PF 2_20

--- blackjack.js
+++ blackjack.js
@@ -1,1 +1,13 @@
 
+
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]);
+        }
+    }
+}
+
