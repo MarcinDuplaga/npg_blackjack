80657b1330ec003883d349182808c99011b900a0
bratres <95509025+bratres@users.noreply.github.com>
2023-05-15 21:44:34 +0200

    PF 2_21

--- blackjack.js
+++ blackjack.js
@@ -11,3 +11,11 @@ function buildDeck() {
     }
 }
 
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
