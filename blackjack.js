b2de99de8f65a37e47c5b7678e6b383af464dc0e
DominikDzialowski <ddzialowski@student.agh.edu.pl>
2023-05-15 22:22:29 +0200

    start gry, rozdanie kart dokończone
    function start game

--- blackjack.js
+++ blackjack.js
@@ -1,1 +1,26 @@
+function startGame() {
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+    dealerAceCount += checkAce(hidden);
+    while (dealerSum < 17) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+        setTimeout(() => cardImg.classList.add('animate'),10);
+    }
+
+
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./cards/" + card + ".png";
+        yourSum += getValue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg);
+        setTimeout(() => cardImg.classList.add('animate'),10);
+    }
+}
 
