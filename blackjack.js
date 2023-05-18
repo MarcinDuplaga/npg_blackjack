92945ae7d1121e4835b4404d2119e4b495c379bb
MarcinDuplaga <129391153+MarcinDuplaga@users.noreply.github.com>
2023-05-18 22:52:31 +0200

    MD_3_3
    Dodanie funkcji odczytującej przekazane argumenty kart jako wartości punktowych

--- blackjack.js
+++ blackjack.js
@@ -29,3 +29,17 @@ function shuffleDeck() {
         deck[j] = temp;
     }
 }
+
+function getValue(card) {
+    let data = card.split("-");
+    let value = data[0];
+
+
+    if (isNaN(value)) {
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
