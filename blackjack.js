341a8a7b0e9b64c3759a09b8a5e964d3d8c95053
DominikDzialowski <ddzialowski@student.agh.edu.pl>
2023-05-19 11:57:05 +0200

    redukcja wartości asa

--- blackjack.js
+++ blackjack.js
@@ -64,4 +64,11 @@ function checkAce(card) {
     return 0;
 }
 
+function reduceAce(playerSum, playerAceCount) {
+    while (playerSum > 21 && playerAceCount > 0) {
+        playerSum -= 10;
+        playerAceCount -= 1;
+    }
+    return playerSum;
+}
 
